6561807d6be9751536f17f933d2a9eeec7becf87
Jay Butera <buterajay@gmail.com>
2017-08-16 10:48:43 -0500

    Finish user wrapper

--- user_engine.py
+++ user_engine.py
@@ -6,8 +6,7 @@ def play_game():
     # Store play information
     db = []
     # Initial rendering
-    #stdscr.addstr(str(env))
-    print(env)
+    stdscr.addstr(str(env))
 
     done = False
     # Global action
@@ -15,22 +14,21 @@ def play_game():
 
     while not done:
         action = 6
-        #key = stdscr.getch()
-        key = input()
+        key = stdscr.getch()
 
         if key == -1: # No key pressed
             action = 6
-        elif key == ord('a'):
+        elif key == ord('a'): # Shift left
             action = 0
-        elif key == ord('d'):
+        elif key == ord('d'): # Shift right
             action = 1
-        elif key == ord('w'):
+        elif key == ord('w'): # Hard drop
             action = 2
-        elif key == ord('s'):
+        elif key == ord('s'): # Soft drop
             action = 3
-        elif key == ord('q'):
+        elif key == ord('q'): # Rotate left
             action = 4
-        elif key == ord('e'):
+        elif key == ord('e'): # Rotate right
             action = 5
 
         # Game step
@@ -38,24 +36,26 @@ def play_game():
         db.append((state,reward,done,action))
 
         # Render
-        print(env)
-        #stdscr.clear()
-        #stdscr.addstr(str(env))
+        stdscr.clear()
+        stdscr.addstr(str(env))
 
     return db
 
-def play_again(stdscr):
-    stdscr.addstr('Play Again? [y/n]')
-    stdscr.addstr('> ', end='')
-    #choice = input()
-    choice = stdscr.getch()
+def play_again():
+    #stdscr.addstr('Play Again? [y/n]')
+    print('Play Again? [y/n]')
+    print('> ', end='')
+    #stdscr.addstr('> ')
+    choice = input()
+    #choice = stdscr.getch()
 
     return True if choice.lower() == 'y' else False
 
-def save_game(stdscr):
-    #print('Would you like to store the game info as training data? [y/n]')
-    stdscr.addstr('Would you like to store the game info as training data? [y/n]')
-    stdscr.addstr('> ', end='')
+def save_game():
+    print('Would you like to store the game info as training data? [y/n]')
+    #stdscr.addstr('Would you like to store the game info as training data? [y/n]\n')
+    #stdscr.addstr('> ')
+    print('> ', end='')
     choice = input()
     return True if choice.lower() == 'y' else False
 
@@ -66,11 +66,9 @@ def terminate():
     curses.endwin()
 
 def init():
-    #stdscr = curses.initscr()
     # Don't display user input
     curses.noecho()
     # React to keys without pressing enter (700ms delay)
-    #curses.cbreak()
     curses.halfdelay(7)
     # Enumerate keys
     stdscr.keypad(True)
@@ -78,25 +76,23 @@ def init():
     #return stdscr
 
 if __name__ == '__main__':
-    #stdscr = curses.initscr()
-    #init(stdscr)
+    # Curses standard screen
+    stdscr = curses.initscr()
 
     # Init environment
     width, height = 10, 20 # standard tetris friends rules
     env = TetrisEngine(width, height)
 
     # Play games on repeat
-    #while True:
-    for i in range(2):
-        #stdscr = curses.initscr()
-        #init()
+    while True:
+        init()
+        stdscr.clear()
         env.clear()
         db = play_game()
 
         # Return to terminal
-        #terminate()
+        terminate()
         # Should the game info be saved?
-        '''
         if save_game():
             with open('training_data.npy', 'ab') as f:
                 print('Saving {0} moves...'.format(len(db)))
@@ -105,7 +101,4 @@ if __name__ == '__main__':
         # Prompt to play again
         if not play_again():
             print('Thanks for contributing!')
-            terminate(stdscr)
             break
-        '''
-        #stdscr = init()
