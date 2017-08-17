8a00d3e701e7e170e2acf1ae9f63da368f43773f
Jay Butera <buterajay@gmail.com>
2017-08-17 02:21:08 -0500

    Supervised model

---
+++ supervised_agent.py
@@ -0,0 +1,51 @@
+from dqn_agent import DQN
+import numpy as np
+import torch.optim as optim
+import torch.nn as nn
+import torch
+from torch.autograd import Variable
+
+FloatTensor = torch.FloatTensor
+LongTensor = torch.LongTensor
+
+def train(X_batch, Y_batch):
+    optimizer.zero_grad()
+
+    preds = model(X_batch)
+    loss = criterion(preds, Y_batch)
+    loss.backward()
+    optimizer.step()
+
+    return loss.data[0]
+
+if __name__ == '__main__':
+    model = DQN()
+    optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+    criterion = nn.CrossEntropyLoss()
+
+    # Load data
+    data = np.load('training_data.npy')
+    X_train = np.stack(data[:,0],
+            axis=0).reshape((len(data),1,len(data[0][0]),len(data[0][0][0])))
+    Y_train = data[:,3]
+
+    # Training loop
+    for epoch in range(100):
+        # Randomize and batch training data
+        batchsize = 4
+        # Randomly shuffle each epoch
+        np.random.shuffle(X_train)
+        np.random.shuffle(Y_train)
+        # Batch
+        X = np.array_split( X_train, batchsize) # States
+        Y = np.array_split( Y_train, batchsize) # Actions
+
+        loss = 0.
+        for X_batch, Y_batch in zip(X,Y):
+            X_batch = Variable(FloatTensor(X_batch), requires_grad=True)
+            Y_batch = Variable(LongTensor(Y_batch), requires_grad=False)
+            loss += train(X_batch, Y_batch)
+
+        if epoch % 10 == 0:
+            print('[{0}] loss: {1}'.format(epoch+1, loss))
+
